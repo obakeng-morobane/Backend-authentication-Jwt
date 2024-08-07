b7abb4f44bf81e8b2b1b4fd8312b338ba27bd708
obakeng-morobane <obakengmorobane@hotmail.com>
2024-08-07 15:48:35 +0200

    first commit
    Signed-off-by: obakeng-morobane <obakengmorobane@hotmail.com>

--- src/main/java/com/example/AuthenticatedBackend/Application.java
+++ src/main/java/com/example/AuthenticatedBackend/Application.java
@@ -33,7 +33,7 @@ public class Application {
 
 			Set<Role> roles = new HashSet<>();
 			roles.add(adminRole);
-
+			String test = "test";
 			User admin = new User(1, "admin", passwordEncoder.encode("password"), roles );
 
 			userRepository.save(admin);
